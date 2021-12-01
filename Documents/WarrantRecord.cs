65e182ec901cc94e86a93799f214d9a3bc48f914
Бальтазар Лолронов <Slavacer@bk.ru>
2021-12-01 11:34:53 +0300

    Комментарии к полям

--- Documents/WarrantRecord.cs
+++ Documents/WarrantRecord.cs
@@ -8,14 +8,41 @@ namespace ASO.Documents
 {
     public class WarrantRecord
     {
+        /// <summary>
+        /// Разработал
+        /// </summary>
         public string DeveloperName { get; private set; }
+        /// <summary>
+        /// Проверил
+        /// </summary>
         public string WhoChecked { get; private set; }
+        /// <summary>
+        /// Нормоконтроль
+        /// </summary>
         public string NormalInspection { get; private set; }
+        /// <summary>
+        /// ГИП
+        /// </summary>
         public string GIP { get; private set; }
+        /// <summary>
+        /// «Шифр тома»-ТЧ
+        /// </summary>
         public string TomeCipher { get; private set; }
+        /// <summary>
+        /// Название документа
+        /// </summary>
         public string DocumentName { get; private set; }
+        /// <summary>
+        /// Стадия
+        /// </summary>
         public string Stage { get; private set; }
+        /// <summary>
+        /// Название компании
+        /// </summary>
         public string CompanyName { get; private set; }
+        /// <summary>
+        /// Главный инженер, фамилия и инициалы (И.О. Фамилия РП)
+        /// </summary>
         public string MainIngenerName { get; private set; }
         /// <summary>
         /// 
@@ -28,7 +55,7 @@ namespace ASO.Documents
         /// <param name="documentName">Наименование документа</param>
         /// <param name="stage">Стадия</param>
         /// <param name="companyName">Название компании</param>
-        /// <param name="mainIngenerName">Главный инженер, фамилия и инициалы</param>
+        /// <param name="mainIngenerName">Главный инженер, фамилия и инициалы (И.О. Фамилия РП)</param>
         public WarrantRecord(string developerName, string whoChecked, string normalInspection, string gip, string tomeCipher, string documentName, string stage, string companyName, string mainIngenerName)
         {
             DeveloperName = developerName;
