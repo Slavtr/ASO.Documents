3e5833ac9c5685a5413d01814d79214e78c6738f
Бальтазар Лолронов <Slavacer@bk.ru>
2021-12-01 11:08:48 +0300

    Класс гарантийной записи

--- Documents/WarrantRecord.cs
+++ Documents/WarrantRecord.cs
@@ -8,6 +8,38 @@ namespace ASO.Documents
 {
     public class WarrantRecord
     {
-        public string DeveloperName { get; set; }
+        public string DeveloperName { get; private set; }
+        public string WhoChecked { get; private set; }
+        public string ContrName { get; private set; }
+        public string GIP { get; private set; }
+        public string TomeCipher { get; private set; }
+        public string DocumentName { get; private set; }
+        public string Stage { get; private set; }
+        public string CompanyName { get; private set; }
+        public string MainIngenerName { get; private set; }
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="developerName">Разработал</param>
+        /// <param name="whoChecked">Проверил</param>
+        /// <param name="contrName"></param>
+        /// <param name="gip"></param>
+        /// <param name="tomeCipher"></param>
+        /// <param name="documentName"></param>
+        /// <param name="stage"></param>
+        /// <param name="compantName"></param>
+        /// <param name="mainIngenerName"></param>
+        public WarrantRecord(string developerName, string whoChecked, string contrName, string gip, string tomeCipher, string documentName, string stage, string compantName, string mainIngenerName)
+        {
+            DeveloperName = developerName;
+            WhoChecked = whoChecked;
+            ContrName = contrName;
+            GIP = gip;
+            TomeCipher = tomeCipher;
+            DocumentName = documentName;
+            Stage = stage;
+            CompanyName = compantName;
+            MainIngenerName = mainIngenerName;
+        }
     }
 }
--- Documents/WordDocument.cs
+++ Documents/WordDocument.cs
@@ -16,15 +16,26 @@ namespace ASO.Documents
             if (document != null)
             {
                 Document doc = _app.Documents.Open(templatePath);
+                Bookmark pagesCount = null;
                 foreach(PropertyInfo i in document.GetType().GetProperties())
                 {
                     foreach (Bookmark b in doc.Bookmarks)
                     {
                         if (b.Name == i.Name)
                         {
+                            Console.WriteLine("В закладку {0} вставлен текст {1}", b.Name, i.GetValue(document, null).ToString());
                             b.Range.Text = i.GetValue(document, null).ToString();
                         }
+                        if(b.Name == "PagesCount")
+                        {
+                            pagesCount = b;
+                        }
                     }
+                }
+                if (pagesCount != null)
+                {
+                    pagesCount.Range.Text = Convert.ToString(doc.ComputeStatistics(WdStatistic.wdStatisticPages, false));
+                    Console.WriteLine("В закладку PagesCount вставлен текст {0}", pagesCount.Range.Text);
                 }
                 if (isOpen)
                 {
